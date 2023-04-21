bbc863f3903f83d5dcd6e9234b3e5d226a431299
Stillo <juan.stillo@sofse.gob.ar>
2023-04-20 22:51:02 -0300

    Pre entrega 1

--- desafios/entrega4/src/routes/carts.router.js
+++ desafios/entrega4/src/routes/carts.router.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import CartManager from '../services/CartManager.js'
+import ProductManager from "../services/ProductManager.js";
+import fs from 'fs';
+
 const router = Router()
 const CartManager1 = new CartManager("carts.json")
+const ProductManager2 = new ProductManager("products.json")
+
 
 router.post('/', async (req,res) => {
     const { id, quantity} = req.body
@@ -28,12 +33,22 @@ router.post('/:cid/product/:pid', async (req, res) => {
     const cId = parseInt(req.params.cid)
     const pId = parseInt(req.params.pid)
     const cart = await CartManager1.getCartById(cId);
-    if (cart) {
-        res.status(200).json(cart)
+    const productFounded = await ProductManager2.getProductById(pId)
+    if (!cart) {
+        res.status(404).send('Cart not Found')
+    }
+    if(!productFounded){
+        res.status(404).send('Product not Found')
+    }
+    const existingProductCart = cart.products.find(p =>p.productId === pId)
+    if(existingProductCart){
+        existingProductCart.quantity++
     }else{
-         const error = {error: 'Cart not found'}
-         return res.status(404).send(error)   
-        }
+        cart.products.push({productId:pId,quantity:1})
+    };
+    await fs.promises.appendFile("../carts.json", JSON.stringify(cart));
+    console.log(cart);
+    res.status(200).send("Cart Updated")
 })
 
 
--- desafios/entrega4/src/services/CartManager.js
+++ desafios/entrega4/src/services/CartManager.js
@@ -36,6 +36,10 @@ class CartManager {
         }
     }
 
+    async updateCart(){
+
+    }
+
 
 
   }
